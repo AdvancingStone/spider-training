0b2560fea5db4b80347d7a5474fb1b273f50fd25
liushuai <banaana@qq.com>
2020-01-14 00:51:34 +0800

    pymongo demo

--- com/bluehonour/db/test_mongo.py
+++ com/bluehonour/db/test_mongo.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo import ASCENDING
 
 conn = MongoClient('mongodb://localhost:27017')
 # 创建一个 avIdol 数据库，如果 mongodb 没有会自行创建
@@ -7,7 +8,11 @@ db = conn.avIdol
 # db.col.insert({"name": '波多野結衣', 'bwh': '{ "b": 90, "w": 59, "h": 85}' , 'age': 30})
 # print(db.col.find_one())
 
-db.col.insert_many([
+# 创建cols集合, 未写自动创建
+coll = db.col1a
+coll.create_index([('name', ASCENDING)], unique=True)
+
+coll.insert_many([
     {"name": '波多野結衣', 'bwh': '{ "b": 90, "w": 59, "h": 85}' , 'age': 30},
     {"name": '吉泽明步', 'bwh': '{ "b": 86, "w": 58, "h": 86}' , 'age': 35},
     {"name": '桃乃木香奈', 'bwh': '{ "b": 80, "w": 54, "h": 80}' , 'age': 22},
@@ -15,13 +20,14 @@ db.col.insert_many([
     {"name": '松下纱荣子', 'bwh': '{ "b": 88, "w": 57, "h": 86}' , 'age': 28}
 ])
 
-for item in db.col.find():
+for item in coll.find():
     print(item)
 
-print(db.col.find({'name' : '松下纱荣子'}))
-db.col.update_many({'name':'吉泽明步'},{'$set':{'name':'苍井空'}})
-db.col.delete_one({'name' : '吉泽明步'})
-db.col.delete_many({'name':'西宫梦'})
+print(coll.find_one({'name' : '松下纱荣子'}))
+
+coll.update_many({'name':'吉泽明步'},{'$set':{'name':'苍井空'}})
+coll.delete_one({'name' : '吉泽明步'})
+coll.delete_many({'name':'西宫梦'})
 # 删除所有数据
-db.col.delete_many({})
+coll.delete_many({})
 
